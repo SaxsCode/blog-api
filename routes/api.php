4c81205f59e58c2273c84f93ab69a8fda6eb216a
SaxsCode <maxsax.sch@gmail.com>
2025-08-16 22:40:55 +0200

    test: improve sanctum test by using a proper route

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,10 @@ use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/authenticated', function () {
+        return true;
+    })->name('authenticated');
+
     Route::post('/article/create', [ArticleController::class, 'store'])->name('article.create');
 });
--- tests/Feature/SanctumTest.php
+++ tests/Feature/SanctumTest.php
@@ -9,16 +9,14 @@ class SanctumTest extends TestCase
     public function test_api_can_access_sanctum_route(): void
     {
         $this->actingAsApi();
-        $response = $this->postJson(route('article.create'));
-
+        $response = $this->getJson(route('authenticated'));
         $response->assertOk();
     }
 
     public function test_guest_can_not_access_sanctum_route(): void
     {
         $this->actingAsGuest();
-        $response = $this->postJson(route('article.create'));
-
+        $response = $this->getJson(route('authenticated'));
         $response->assertUnauthorized();
     }
 }
