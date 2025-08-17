73b9e3ebf13cb7495585275b879587bc0a49e590
SaxsCode <maxsax.sch@gmail.com>
2025-08-17 21:37:56 +0200

    test: add test for update functionality in article class

--- tests/Feature/ArticleTest.php
+++ tests/Feature/ArticleTest.php
@@ -7,6 +7,8 @@ use Tests\TestCase;
 
 class ArticleTest extends TestCase
 {
+    private const INVALID_ID = 999;
+
     protected function setUp(): void
     {
         parent::setUp();
@@ -52,13 +54,20 @@ class ArticleTest extends TestCase
 
     public function test_can_retrieve_specific_article(): void
     {
-        $postResponse = $this->postArticle();
-        $articleId = $postResponse->json('article.id');
+        $article = $this->postArticle();
+        $articleId = $article->json('article.id');
 
         $response = $this->getJson(route('articles.show', ['article' => $articleId]));
 
         $response->assertOk()
             ->assertJsonFragment($this->validArticleData());
+    }
+
+    public function test_returns_404_retrieving_non_existant_article(): void
+    {
+        $response = $this->getJson(route('articles.show', ['article' => self::INVALID_ID]));
+
+        $response->assertNotFound();
     }
 
     public function test_can_create_article(): void
@@ -75,5 +84,37 @@ class ArticleTest extends TestCase
         $response = $this->postArticle($this->invalidArticleData());
 
         $response->assertUnprocessable();
+    }
+
+    public function test_can_update_article(): void
+    {
+        $article = $this->postArticle();
+        $articleId = $article->json('article.id');
+        $updatedArticle = ['title' => 'Updated title'];
+
+        $response = $this->patchJson(route('articles.update', ['article' => $articleId]), $updatedArticle);
+
+        $response->assertOk()
+            ->assertJsonFragment($updatedArticle);
+    }
+
+    public function test_can_not_update_article_with_invalid_data(): void
+    {
+        $article = $this->postArticle();
+        $articleId = $article->json('article.id');
+        $updatedArticle = ['title' => self::INVALID_ID];
+
+        $response = $this->patchJson(route('articles.update', ['article' => $articleId]), $updatedArticle);
+
+        $response->assertUnprocessable();
+    }
+
+    public function test_can_not_update_non_existant_article(): void
+    {
+        $updatedArticle = ['title' => 'Updated title'];
+
+        $response = $this->patchJson(route('articles.update', ['article' => self::INVALID_ID]), $updatedArticle);
+
+        $response->assertNotFound();
     }
 }
